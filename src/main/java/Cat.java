e74debbef1d787b9d4c4429ab77f55b61f32f256
IGOR0SHAM <igor.sgride@yandex.ru>
2023-09-26 17:47:19 +0500

    Test

---
+++ src/main/java/Cat.java
@@ -0,0 +1,18 @@
+public class Cat {
+    private String massage;
+
+    public String getMassage() {
+        return massage;
+    }
+
+    public void setMassage(String massage) {
+        this.massage = massage;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "massage='" + massage + '\'' +
+                '}';
+    }
+}
